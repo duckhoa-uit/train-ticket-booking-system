13d73ca8f5599e86186a74a13acb32881f39cad3
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-11-03 14:46:46 +0700

    refactor(footer): refactor code in Footer

---
+++ apps/www/src/app/Components/Footer/item.ts
@@ -0,0 +1,53 @@
+export const about = {
+  title: "Về chúng tôi",
+  links: [
+    {
+      name: "Giới thiệu",
+      link: "#",
+    },
+    {
+      name: "Sự nghiệp",
+      link: "#",
+    },
+
+    {
+      name: "Blog",
+      link: "#",
+    },
+    {
+      name: "Liên hệ",
+      link: "#",
+    },
+  ],
+};
+
+export const support = {
+  title: "Hỗ trợ khách hàng",
+  links: [
+    {
+      name: "Điều khoản & Điều kiện",
+      link: "#",
+    },
+    {
+      name: "Hướng dẫn thanh toán",
+      link: "#",
+    },
+
+    {
+      name: "Chính sách thanh toán",
+      link: "#",
+    },
+    {
+      name: "Chính sách hoàn trả",
+      link: "#",
+    },
+    {
+      name: "Chính sách mua hàng",
+      link: "#",
+    },
+    {
+      name: "Chích sách bảo mật thông tin",
+      link: "#",
+    },
+  ],
+};
