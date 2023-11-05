b9b2fada33eb0c2b3342088157ca12971b3e11fe
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-11-05 14:45:42 +0700

    style(tailwind.config.ts): set base colors

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -1,13 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+
 
 const config: Config = {
-  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      primary: "#F8F9FA",
+      secondary: "#174576",
+      accent: "#B57617",
+    },
+    textColor: {
+      primaryTextColor: "#2C3640",
+      secondaryTextColor: "#F8F9FA",
     },
   },
   plugins: [],
