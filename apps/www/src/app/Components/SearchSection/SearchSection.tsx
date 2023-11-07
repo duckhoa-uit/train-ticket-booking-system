e7340580e9326f9de94439378a2fd9a560c03b87
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-11-07 16:40:46 +0700

    style(SearchBar, SearchSection): design UI, add responsive for mobile, pc version

--- apps/www/src/app/Components/SearchSection/SearchSection.tsx
+++ apps/www/src/app/Components/SearchSection/SearchSection.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import React from "react";
 
 import SearchBar from "./SearchBar";
 
 const SearchSection = () => {
   return (
-    <div className="bg-train relative z-[-1000] h-[500px] w-full bg-cover bg-center bg-no-repeat font-bold">
-      <p className="text-secondary box-border block text-center text-2xl">
+    <div className="bg-train flex h-auto w-full flex-col items-center justify-center gap-3 bg-cover bg-center bg-no-repeat p-10 font-bold">
+      <p className="text-secondary box-border text-center text-2xl">
         WaysToYourHeart - Guarantee 150% refund if transport service is not provided
       </p>
       <SearchBar />
