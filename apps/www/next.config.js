39d84d68c1916eaab87ac8b9e54e94531283fe73
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-11-07 14:35:05 +0700

    fix(Header, Footer): - Fix bugs: menu in header, keys in footer

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ["@ttbs/lib", "@ttbs/ui", "@ttbs/i18n"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
